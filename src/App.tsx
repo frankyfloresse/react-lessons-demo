a8447f0ad1dbca8fcf49fc2d3ce23b2cd3d106ed
frankyfloresse <foz.fox2015@gmail.com>
2024-12-29 19:50:52 +0200

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,66 @@
-import './App.css'
+// const App = () => {
+//     let counter = 0;
+//     console.log('mount')
+//
+//     return (
+//         <div>
+//             <h2>{counter}</h2>
+//             <button onClick={() => {
+//                 ++counter;
+//                 console.log('counter')
+//             }}>increment</button>
+//
+//             <button onClick={() => {
+//                 --counter;
+//                 console.log('counter')
+//             }}>decrement</button>
+//         </div>
+//     );
+// };
 
-function App() {
 
-  return (
-      <>
-      </>
-  )
-}
+import {useState} from "react";
 
-export default App
+// const App = () => {
+//
+//     //eslint-disable-next-line prefer-const
+//     let [counter, setCounter] = useState<number>(0);
+//     console.log('mount')
+//
+//     return (
+//         <div>
+//             <h2>{counter}</h2>
+//             <button onClick={() => {
+//                 setCounter(++counter);
+//             }}>increment</button>
+//
+//             <button onClick={() => {
+//                 setCounter(--counter);
+//             }}>decrement</button>
+//         </div>
+//     );
+// };
+
+const App = () => {
+
+    //eslint-disable-next-line prefer-const
+    let [counter, setCounter] = useState<number>(0);
+    // console.log('mount')
+
+    return (
+        <div>
+            <h2>{counter}</h2>
+            <button onClick={() => {
+                setCounter(++counter);
+            }}>increment</button>
+
+            <button onClick={() => {
+                setCounter(prevState => {
+                    console.log(prevState);
+                    return --prevState;
+                });
+            }}>decrement</button>
+        </div>
+    );
+};
+export default App;
