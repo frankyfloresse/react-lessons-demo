6c6277430f1517269fd946765dc8abdbe0d851b2
frankyfloresse <foz.fox2015@gmail.com>
2025-01-15 19:44:07 +0200

    add lesson 5/axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import './App.css'
+import {useEffect} from "react";
+import {getAllUsers, saveUser} from "./services/user.service.ts";
 
 function App() {
+    useEffect(() => {
+        getAllUsers().then(value => console.log(value));
+
+        saveUser({id: 1, name: 'John', email: 'sdfghjk'}).then(value => console.log(value));
+    }, []);
 
   return (
       <>
---
+++ src/services/user.service.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import {IUser} from "../models/IUser.ts";
+
+const axiosInstance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com/',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+axiosInstance.interceptors.request.use((request) => {
+    request.headers.set('Authorization', 'todo');//додавати додаткові хедери з ключами доступу
+    console.log(request.method);
+    return request;
+})
+
+axiosInstance.interceptors.response.use(
+    r => r,
+    (error) => {
+        if (error.message) {
+            alert(error.message)
+        }
+    }
+)
+
+export const getAllUsers = async () => {
+    const { data } = await axiosInstance.get<IUser[]>('/users');
+    return data;
+}
+
+export const saveUser = async (user: IUser) => {
+    const { data } = await axiosInstance.post<IUser>('/users', user);
+    return data;
+}
