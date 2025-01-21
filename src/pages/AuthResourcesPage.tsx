15b4b85b1a8b798343664c7b5a76ab89dd75870c
frankyfloresse <foz.fox2015@gmail.com>
2025-01-21 20:45:27 +0200

    add lesson 6/auth demo

--- src/pages/AuthResourcesPage.tsx
+++ src/pages/AuthResourcesPage.tsx
@@ -1,20 +1,7 @@
-import {useEffect} from "react";
-import {loadAuthProducts, refresh} from "../services/api.service.ts";
+
 
 const AuthResourcesPage = () => {
-    useEffect(() => {
-        // Attempts to load products; if successful, logs them
-        loadAuthProducts().then(products => {
-            console.log(products);
-            // If loading products fails (due to expired tokens), it catches the error, logs it
-        }).catch(reason => {
-            console.log(reason);
-            // attempts to refresh tokens and retry loading products
-            refresh()
-                .then(() => loadAuthProducts())
-                .then(value => console.log(value));
-        })
-    }, []);
+
 
     return (
         <div>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,15 +1,8 @@
-import {useEffect} from "react";
-import {login} from "../services/api.service.ts";
+
 
 
 const LoginPage = () => {
-    useEffect(() => {
-        login({
-            username: 'emilys',
-            password: 'emilyspass',
-            expiresInMins: 1
-        }) //triggers a login request. On successful login, user authentication tokens are stored in localStorage
-    }, []);
+
 
     return (
         <div>
