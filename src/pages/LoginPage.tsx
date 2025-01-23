fd9ce089810107ebdba8b0d21f451596d6d94b9d
frankyfloresse <foz.fox2015@gmail.com>
2025-01-23 15:14:08 +0200

    add lesson 6/auth demo

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,20 +1,46 @@
-import {useEffect} from "react";
 import {login} from "../services/api.service.ts";
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
 
+const schema = z.object({
+    username: z.string().min(1, { message: 'Required' }),
+    password: z.string().min(10),
+});
+
+interface LoginForm {
+    username: string;
+    password: string;
+}
 
 const LoginPage = () => {
-    useEffect(() => {
-        login({
-            username: 'emilys',
-            password: 'emilyspass',
-            expiresInMins: 1
-        })
-    }, []);
+    const {handleSubmit, register, formState: { errors }} = useForm<LoginForm>({
+        resolver: zodResolver(schema),
+    })
 
+    const onSubmit = async ({ username, password }: LoginForm) => {
+        try {
+            await login({
+                username,
+                password,
+                expiresInMins: 1
+            })
+            // User is authed, navigate to user personal profile page
+        }
+        catch {
+            console.log('Auth failed')
+        }
+    }
 
     return (
         <div>
-            LoginPage content
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register('username')} />
+                {errors.username?.message && <p>{errors.username?.message}</p>}
+                <input type="text" {...register('password')} />
+                {errors.password?.message && <p>{errors.password?.message}</p>}
+                <input type="submit" />
+            </form>
         </div>
     );
 };
