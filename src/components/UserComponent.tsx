1f896937109408e0f71bd2ed7cdd177e897e7796
frankyfloresse <foz.fox2015@gmail.com>
2025-01-23 18:43:19 +0200

    add lesson 7/custom hooks

--- src/components/UserComponent.tsx
+++ src/components/UserComponent.tsx
@@ -1,12 +1,12 @@
 import {FC, memo} from "react";
 
 
-const UserComponent: FC<{ foo: () => void, arrNums: number[] }> = memo(({ arrNums }) => { //не буде рендиритися, якщо не буде змінюватися
+const UserComponent: FC<{ foo: () => void, arrNums: number[], item: {name: string} }> = memo(({ arrNums, item }) => { //не буде рендиритися, якщо не буде змінюватися
     console.log('user');
     console.log(arrNums);
 
     return (
-        <div>user component</div>
+        <div>{item.name}</div>
     );
 });
 
--- src/components/UsersComponent.tsx
+++ src/components/UsersComponent.tsx
@@ -1,34 +1,27 @@
 import UserComponent from "./UserComponent.tsx";
-import {useCallback, useEffect, useMemo, useState} from "react";
+import {useCallback, useMemo} from "react";
+import {useFetch} from "../hooks/useFetch.tsx";
 
 
 const UsersComponent = () => {
     console.log('users')
+    const users = useFetch();
 
     const foo = useCallback(() => { //кешувати пропси userComponent
         console.log('foo')
     }, [])
 
-    const [users, setUsers] = useState([])
-
     const arr: number[] = useMemo(() => {
         return [11, 22, 33];
     }, []);
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(value => value.json())
-            .then(value => setUsers(value))
 
-        return () => {
-            console.log('unsubscribe');
-        }
-
-    }, []);
     return (
         <div>
             users component
-            <UserComponent foo={foo} arrNums={arr} />
+            {
+                users.map(value => <UserComponent item={value} foo={foo} arrNums={arr} /> )
+            }
         </div>
     );
 };
