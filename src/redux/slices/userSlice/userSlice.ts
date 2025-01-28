7821e06c29dc6cc19be233a37a6bcc347254cc49
frankyfloresse <foz.fox2015@gmail.com>
2025-01-29 01:30:43 +0200

    add lesson 8/redux asyncThunk, addMatcher

--- src/redux/slices/userSlice/userSlice.ts
+++ src/redux/slices/userSlice/userSlice.ts
@@ -1,12 +1,16 @@
 import {IUser} from "../../../models/IUser.ts";
-import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, isFulfilled, isRejected, PayloadAction} from "@reduxjs/toolkit";
 
 type UserSliceType = {
     users: IUser[];
+    user: IUser | null;
+    loadState: boolean;
 }
 
 const initialState: UserSliceType = {
     users: [],
+    user: null,
+    loadState: false
 };
 
 const loadUsers = createAsyncThunk(
@@ -14,10 +18,10 @@ const loadUsers = createAsyncThunk(
     async (_, thunkAPI) => {
         try {
             const users = await fetch('https://jsonplaceholder.typicode.com/users')
-                .then(value => value.json())
-            return thunkAPI.fulfillWithValue(users);
+                .then(value => value.json());
+            thunkAPI.dispatch(userSliceActions.changeLoadState(true));
 
-            // throw new Error();
+            return thunkAPI.fulfillWithValue(users);
 
         } catch (e) {
             console.log(e);
@@ -25,20 +29,51 @@ const loadUsers = createAsyncThunk(
         }
     })
 
+const loadUser = createAsyncThunk(
+    'userSlice/loadUser',
+    async (id: string, thunkAPI) => {
+        try {
+            const user = await fetch('https://jsonplaceholder.typicode.com/users/' + id)
+                .then(value => value.json());
+            thunkAPI.dispatch(userSliceActions.changeLoadState(true));
+
+            return thunkAPI.fulfillWithValue(user);
+
+        } catch (e) {
+            console.log(e);
+            return thunkAPI.rejectWithValue('some error')
+        }
+    })
+
+
 export const userSlice = createSlice({
     name: 'userSlice',
     initialState: initialState,
-    reducers: {},
+    reducers: {
+        changeLoadState: (state, action: PayloadAction<boolean>) => {
+            state.loadState = action.payload;
+        }
+    },
     extraReducers: builder => builder
-            .addCase(loadUsers.fulfilled, (state, action: PayloadAction<IUser[]>) => {
+        .addCase(loadUsers.fulfilled, (state, action: PayloadAction<IUser[]>) => {
             state.users = action.payload;
         })
-            .addCase(loadUsers.rejected, (state, action) => {
+        .addCase(loadUsers.rejected, (state, action) => {
                 console.log(state);
                 console.log(action);
-            })
+        })
+        .addCase(loadUser.fulfilled, (state, action: PayloadAction<IUser>) => {
+            state.user = action.payload;
+        })
+        .addMatcher(isFulfilled(loadUser, loadUsers), (state) => {
+            state.loadState = true;
+        })
+        .addMatcher(isRejected(loadUsers, loadUser), (state) => {
+            console.log(state);
+        })
 });
 export const userSliceActions = {
     ...userSlice.actions,
-    loadUsers
+    loadUsers,
+    loadUser
 }
